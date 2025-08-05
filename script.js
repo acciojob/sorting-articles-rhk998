1cadcba93f7f4a31e8d09973cf41718be32258e5
rhk998 <130037816+rhk998@users.noreply.github.com>
2025-08-05 12:51:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+const sortedBands = bands.sort((a, b) => {
+  const cleanA = a.replace(/^(a|an|the)\s/i, '');
+  const cleanB = b.replace(/^(a|an|the)\s/i, '');
+  return cleanA.localeCompare(cleanB);
+});
+
+const ul = document.createElement('ul');
+ul.id = 'band';
+
+sortedBands.forEach(band => {
+  const li = document.createElement('li');
+  li.textContent = band;
+  ul.appendChild(li);
+});
+
+document.body.appendChild(ul);
